20ed4112b9ab846bf188c1aa48c14fe0fa4f15e9
rcarondw <r.carondw@gmail.com>
2021-06-29 13:34:46 +0200

    sidebar done nav bar done

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-import { UserCircleIcon } from '@heroicons/react/solid'
-
+import { UserCircleIcon } from '@heroicons/react/solid';
 
 export default function Navbar() {
-    return(
-        <div className="w-full h-14 bg-gray-800 border-b border-white fixed top-0">
-            <p>TFD</p>
-            <UserCircleIcon className="text-gray-200 w-5"/>
-        </div>
-    )
-};
+  return (
+    <div className="w-full h-14 bg-gray-800 border-b border-white fixed top-0 flex justify-between items-center px-5">
+      <p className="text-2xl text-white font-bold">TFD</p>
+      <UserCircleIcon className="text-gray-200 w-7" />
+    </div>
+  );
+}
