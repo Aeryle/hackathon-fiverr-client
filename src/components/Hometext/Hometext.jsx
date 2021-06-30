c53b06ca5bdafae9609553f8f1e13ed89d5404ca
rcarondw <r.carondw@gmail.com>
2021-06-30 13:41:17 +0200

    remove les copines

--- src/components/Hometext/Hometext.jsx
+++ src/components/Hometext/Hometext.jsx
@@ -7,7 +7,7 @@ export default function Hometext() {
         <h1 className="text-5xl font-extrabold border-b-2 text-gray-800 border-gray-800">Welcome to the best website ever... toujours</h1>
         <input
           type="text"
-          placeholder="It doesn't work... Aller la les copines !"
+          placeholder="It doesn't work... !"
           className="w-full mt-10 py-4 rounded-md px-2 text-xl focus:outline-none border border-gray-800"
         />
       </div>
