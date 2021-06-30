19a053faf0177e5bd35772f057aae502e82822f6
CHABAT ESTELLE <chabat.estelle@gmail.com>
2021-06-30 11:55:30 +0200

    Search Bar in progress

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,16 +7,19 @@ import OneUser from '../Users/OneUser';
 import Users from '../Users/Users';
 import Login from '../Login/Login';
 import SignInForm from '../SignInForm/SignInForm';
+import SearchInput from '../SearchInput';
 
 export default function Layout() {
   return (
     <BrowserRouter>
       <Navbar />
       <Sidebar />
-      <OneUser />
+
       <div className="mt-14 ml-14">
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            <SearchInput />
+          </Route>
           <Route exact path="/users" component={Users} />
           <Route exact path="/users/:id" component={OneUser} />
           <Route exact path="/login">
