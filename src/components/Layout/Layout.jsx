78b28ac20968c1c48324dcbda023c886b5184875
rcarondw <r.carondw@gmail.com>
2021-06-30 10:04:11 +0200

    remove searchinput

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,7 +7,6 @@ import OneUser from '../Users/OneUser';
 import Users from '../Users/Users';
 import Login from '../Login/Login';
 import SignInForm from '../SignInForm/SignInForm';
-import SearchInput from '../SearchInput/SearchInput';
 
 export default function Layout() {
   return (
@@ -17,9 +16,7 @@ export default function Layout() {
       <OneUser />
       <div className="mt-14 ml-14">
         <Switch>
-          <Route exact path="/">
-            <SearchInput />
-          </Route>
+          <Route exact path="/"></Route>
           <Route exact path="/users" component={Users} />
           <Route exact path="/users/:id" component={OneUser} />
           <Route exact path="/login">
